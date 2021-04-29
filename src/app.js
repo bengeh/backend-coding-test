c31882082b231755e65302651b46405eff78f87e
1564479 <Benjamin.Geh@sc.com>
2021-04-29 15:32:01 +0800

    added artillery and convert callback to async

--- src/app.js
+++ src/app.js
@@ -5,14 +5,31 @@ const app = express();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const winston = require('winston')
-const logConfiguration = require('./logger')
+const logConfiguration = require('./logger');
+const { json } = require('body-parser');
 
 const logger = winston.createLogger(logConfiguration);
 
+async function query(sql, params) {
+    const connection = await mysql.createConnection(config.db);
+    const [results, ] = await connection.execute(sql, params);
+  
+    return results;
+  }
+  
 module.exports = (db) => {
-    app.get('/health', (req, res) => res.send('Healthy'));
+    app.get('/health', async (req, res) => {
+        try {
+            return res.json({'status': 'healthy'})
+        } catch (err) {
+            return res.send({
+                error_code: err,
+                message: 'Service down'
+            });
+        }
+    });
 
-    app.post('/rides', jsonParser, (req, res) => {
+    app.post('/rides', jsonParser, async (req, res) => {
         logger.warn("Hello, Winston logger, the first warning!");
         const startLatitude = Number(req.body.start_lat);
         const startLongitude = Number(req.body.start_long);
@@ -58,70 +75,43 @@ module.exports = (db) => {
         }
 
         var values = [req.body.start_lat, req.body.start_long, req.body.end_lat, req.body.end_long, req.body.rider_name, req.body.driver_name, req.body.driver_vehicle];
-        
-        const result = db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values, function (err) {
-            if (err) {
-                return res.send({
-                    error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
-                });
-            }
-
-            db.all('SELECT * FROM Rides WHERE rideID = ?', this.lastID, function (err, rows) {
-                if (err) {
-                    return res.send({
-                        error_code: 'SERVER_ERROR',
-                        message: 'Unknown error'
-                    });
-                }
-
-                res.send(rows);
+        try {
+            let result = await db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values)
+            return res.json(result)
+        } catch (err) {
+            return res.send({
+                error_code: 'SERVER_ERROR',
+                message: 'Unknown error'
             });
-        });
+        }
     });
 
-    app.get('/rides', (req, res) => {
-        db.all('SELECT * FROM Rides', function (err, rows) {
-            if (err) {
-                return res.send({
-                    error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
-                });
-            }
+    
+    app.get('/rides', async (req, res) => {
+        try {
+            let rides = await db.all('SELECT * FROM Rides')
+            return res.json(rides)
+        } catch (err) {
+            return res.send({
+                error_code: err,
+                message: 'Could not find any rides'
+            });
+        }
+    })
 
-            if (rows.length === 0) {
-                logger.error("error no rides found...")
-                return res.send({
-                    error_code: 'RIDES_NOT_FOUND_ERROR',
-                    message: 'Could not find any rides'
-                });
-            }
-
-            res.send(rows);
-        });
-    });
-
-    app.get('/rides/:id', (req, res) => {
+    app.get('/rides/:id', async (req, res) => {
         //santizing input, assume id is purely number (security improvement)
         if(/^\d+$/.test(req.params.id)){
-            db.all(`SELECT * FROM Rides WHERE rideID=?`, req.params.id, function (err, rows) {
-                if (err) {
-                    return res.send({
-                        error_code: 'SERVER_ERROR',
-                        message: 'Unknown error'
-                    });
-                }
-    
-                if (rows.length === 0) {
-                    return res.send({
-                        error_code: 'RIDES_NOT_FOUND_ERROR',
-                        message: 'Could not find any rides'
-                    });
-                }
-    
-                res.send(rows);
-            });
-        } else {
+            try{
+                let singleRide = await db.all(`SELECT * FROM Rides WHERE rideID=?`, [req.params.id])
+                return res.json(singleRide)
+            } catch(err) {
+                return res.send({
+                    error_code: 'Service error',
+                    message: 'Service error'
+                });
+            }
+        }else {
             return res.send({
                 error_code: 'INPUT_ERROR',
                 message: 'Input error'
